a026df7df21c3fc678442097e11e14c06f03ffc7
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-02 23:10:56 -0500

    [UPDATE]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,6 +2,6 @@
 # Install Nginx
 
 exec {'Install nginx':
-  command => 'sudo apt-get update && sudo apt-get -y install nginx && sudo sed -i "15i add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.cfg && sudo service nginx restart',
+  command => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "15i add_header X-Served-By \$HOSTNAME;" /etc/nginx/nginx.cfg && sudo service nginx restart',
     provider => shell,
     }
