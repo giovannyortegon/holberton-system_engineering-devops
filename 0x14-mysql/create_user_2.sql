1929d0eb65dfa79c805e42b81f4eebc0b8783e62
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-23 22:40:15 -0500

    [FIX] Flush

--- 0x14-mysql/create_user_2.sql
+++ 0x14-mysql/create_user_2.sql
@@ -1,6 +1,3 @@
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'replica_user';
-GRANT REPLICATION SLAVE ON *.*
-TO 'replica_user'@'%'
-IDENTIFIED BY 'replica_user';
-UPDATE mysql.user SET Repl_slave_priv='N' WHERE user='holberton_user';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replica_user';
 FLUSH PRIVILEGES;
+UPDATE mysql.user SET Repl_slave_priv='N' WHERE user='holberton_user';
