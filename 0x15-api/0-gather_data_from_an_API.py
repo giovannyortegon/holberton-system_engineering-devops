7644036dc724b9e17afe8da04a2c038b8cc36636
vagrant <vagrant@kali-lenovo330s>
2020-02-03 17:32:14 -0800

    [ADD] File 01

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 
-import requests
+from requests import get
 from sys import argv
 
+
 def main(value):
-    
-    data = {'userId': value}
-    
-    url = 'https://jsonplaceholder.typicode.com/todos' 
+    data = {'userId': value[1]}
 
-    req = request(url, params=data)
-    resp = req.json()
+    url_1 = 'https://jsonplaceholder.typicode.com/todos'
 
-    print(resp)
+    req_1 = get(url_1, params=data)
+    resp_1 = req_1.json()
+
+    url_2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(value[1])
+    req_2 = get(url_2)
+    resp_2 = req_2.json().get('name')
+    tasks = []
+
+    for task in resp_1:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+    if resp_2 is not None:
+        print('Employee {} is done with tasks({}/{}):'.format(
+               resp_2, len(tasks), len(resp_1)))
+        for title in tasks:
+            print('\t {}'.format(title))
 
 
 if __name__ == '__main__':
-    main(argv[1])
+    main(argv)
