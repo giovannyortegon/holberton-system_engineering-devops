7d7beddc9b8e51c13cabbb7a4631a57942a665a1
Skillhh <ortegon.giovanny@hotmail.com>
2020-02-26 15:00:41 -0500

    [ADD] File 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Modife holberton Hard and Soft
+exec { 'Hard and Soft':
+  command  => 'sed -i \'s/nofile 5/nofile 3000/; s/nofile 4/nofile 2000/\' /etc/security/limits.conf',
+  provider => shell,
+  }
