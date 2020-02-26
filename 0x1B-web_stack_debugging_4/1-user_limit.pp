9090f15ac5d880709ec20c0f2afbf33327da8814
Skillhh <ortegon.giovanny@hotmail.com>
2020-02-26 15:02:43 -0500

    [UPDATE] Spelling

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# Modife holberton Hard and Soft
+# Modify holberton Hard and Soft
 exec { 'Hard and Soft':
   command  => 'sed -i \'s/nofile 5/nofile 3000/; s/nofile 4/nofile 2000/\' /etc/security/limits.conf',
   provider => shell,
